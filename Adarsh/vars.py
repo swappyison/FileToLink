87c6089f456d45fdcf72908d257ee421012644e5
swappyison <88504971+swappyison@users.noreply.github.com>
2023-11-22 22:05:01 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -36,5 +36,5 @@ class Var(object):
     else:
         URL = "http://{}/".format(FQDN)
     DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://swapnilupadhyaykratos:V9eFyv3M9Bd1rXkf@cluster0.wsrqmd7.mongodb.net/?retryWrites=true&w=majority'))
-    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'nayatorrentbot'))
+    UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', 'uploaderz123'))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
